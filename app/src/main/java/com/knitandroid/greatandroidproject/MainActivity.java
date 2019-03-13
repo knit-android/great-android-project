3dde115b1f878bcec379fc23160128a17ac86262
leedwon <leedwon12@gmail.com>
2019-03-13 11:20:16 +0100

    switch to androidx, add material desing, add simple login layout

--- app/src/main/java/com/knitandroid/greatandroidproject/MainActivity.java
+++ app/src/main/java/com/knitandroid/greatandroidproject/MainActivity.java
@@ -2,15 +2,19 @@ package com.knitandroid.greatandroidproject;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import com.knitandroid.greatandroidproject.databinding.ActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity {
     Context appContex;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         appContex=this;
-
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
